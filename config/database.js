7b584b5287e87b9584337310c71af443d6546c1a
AlexVachon <118298420+AlexVachon@users.noreply.github.com>
2024-11-22 10:11:21 -0500

    Update database.js
    -authenticate

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -10,16 +10,9 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'mariadb',
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        logging: false,
     }
-)
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connexion à la base de données réussie!');
-  })
-  .catch((err) => {
-    console.error('Impossible de se connecter à la base de données:', err);
-  });
+);
 
 module.exports = sequelize;
