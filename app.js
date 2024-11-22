f4421f175dccb7079192c7cd7fd09a29bcdafd8d
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-11-22 14:18:04 -0500

    Correction console logs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const sequelize = require('./config/database');
+const express = require("express");
+const dotenv = require("dotenv");
+const sequelize = require("./config/database");
 
 dotenv.config();
 
@@ -8,22 +8,24 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 const userRoutes = require("./routes/user");
-const versionRoutes = require('./routes/version')
+const versionRoutes = require("./routes/version");
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
-app.use('/api/version', versionRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/version", versionRoutes);
 
 // Connexion à la base de données et démarrage du serveur
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connexion à la base de données réussie!');
+    console.clear();
+    console.log("Connexion à la base de données réussie!");
     app.listen(PORT, () => {
-      console.clear();
       console.log(`Serveur démarré sur http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('Impossible de se connecter à la base de données:', err);
+    console.clear();
+    console.error("Impossible de se connecter à la base de données:", err);
     process.exit(1);
   });
